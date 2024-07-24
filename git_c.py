c80fb52ee95d66cf4e5f8974e8044bb0378119c4
soyluisdev <luisfragoso_2.0@hotmail.it>
2024-07-23 23:07:57 -0600

    First version

--- git_c.py
+++ git_c.py
@@ -5,7 +5,7 @@ for i in range(1, 750):
 
     for j in range(0,randint(1,10)):
         d = str(i) + ' days ago'
-        with open('file.txt', 'a') as file:
+        with open('legend.txt', 'a') as file:
             file.write(d)
         os.system('git add .')
         os.system('git commit --date="' + d + '" -m "coomit"')
